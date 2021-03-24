91b0442787b7de6bd7f8cfca4252de02467fb40b
Bernhard Enders <bgeneto@users.noreply.github.com>
2021-03-24 08:53:22 -0300

    Add files via upload

---
+++ Firefox/content.js
@@ -0,0 +1,36 @@
+(async function () {
+    var url = window.location.href;
+    // aprender3 action
+    if (url.includes("aprender3.unb.br/course/view.php?id=")) {
+        var contents = window.document.body.innerHTML;
+        var pattern = /sesskey=([\w-]{10})/i;
+        var results = contents.match(pattern);
+        if (results) {
+            let sesskey = results[1];
+            let sesskey_store = await browser.storage.local.get("sesskey_store");
+            if (sesskey_store.sesskey_store != sesskey) {
+                browser.storage.local.set({'sesskey_store':sesskey});
+                window.location.replace(url+"&sesskey="+sesskey+"&edit=on");
+            }
+        }
+        return;
+    }
+    // sigaa action
+    if (window.location.href.includes("sig.unb.br/sigaa/ava")) {
+        var contents = window.document.body.innerHTML;
+        var divelem = window.document.getElementById('divEmails');
+        if (divelem != null) {
+            var pattern = /Matr.+cula: <em>\s*(?<mat>\d{9})\s*</gi;
+            var results = contents.matchAll(pattern);
+            divelem.innerHTML = "emails<br>";
+            for (let result of results) {
+                let {all, mat} = result.groups;
+                let email = mat+"@aluno.unb.br<br>";
+                divelem.innerHTML += email;
+            }
+            divelem.innerHTML += "<br>";
+            divelem.style.display = "block";
+        }
+    }
+})();
+
